4999226b7fd7c7453485c754ef5f5789d4a9aa37
Matt Rhone <kritoke@gmail.com>
2017-10-31 21:14:16 -0500

    Add test for making sure content changes if new feed loaded

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -9,18 +9,7 @@
  * to ensure they don't run until the DOM is ready.
  */
 $(function() {
-    /* This is our first test suite - a test suite just contains
-     * a related set of tests. This suite is all about the RSS
-     * feeds definitions, the allFeeds variable in our application.
-     */
     describe('RSS Feeds', function() {
-        /* This is our first test - it tests to make sure that the
-         * allFeeds variable has been defined and that it is not
-         * empty. Experiment with this before you get started on
-         * the rest of this project. What happens when you change
-         * allFeeds in app.js to be an empty array and refresh the
-         * page?
-         */
         it('are defined', function() {
             expect(allFeeds).toBeDefined();
             expect(allFeeds.length).not.toBe(0);
@@ -68,15 +57,24 @@ $(function() {
             expect($('.feed').children().length).toBeGreaterThan(0);
             done();
         });
-
     });
 
     describe('New Feed Selection', function() {
+        let currFeed;
 
+        // load first feed
+        beforeEach(function(done) {
+            loadFeed(0, function() {
+                currFeed = $('.feed').html();
+                done();
+            });
+        });
+
+        it('make sure content changes when new feed is loaded by loadFeed', function(done) {
+            loadFeed(1, function(done) {
+                expect($('.feed').html()).not.toEqual(currFeed);
+                done();
+            })
+        });
     });
-
-    /* TODO: Write a test that ensures when a new feed is loaded
-     * by the loadFeed function that the content actually changes.
-     * Remember, loadFeed() is asynchronous.
-     */
 }());
